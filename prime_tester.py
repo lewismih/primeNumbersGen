428dffada0fa211893fb253a06deca3306fbe40d
lewismih <lewismih@gmail.com>
2017-01-19 15:13:50 +0300

    Tests for prime numbers update

--- prime_tester.py
+++ prime_tester.py
@@ -7,25 +7,26 @@ class TestIsPrime(unittest.TestCase):
     '''Test the prime_num function.'''
     def test_primes(self):
         '''Test that several primes are prime.'''
-        self.assertTrue(prime_num(13))
-        self.assertTrue(prime_num(23))
-        self.assertTrue(prime_num(17))
+        self.assertTrue(prime_num(13) != False)
+        self.assertTrue(prime_num(23) != False)
+        self.assertTrue(prime_num(17) != False)
+        
     def test_negative(self):
         '''Test that a negative number is not prime.'''
-        self.assertFalse(prime_num(-20))
-        self.assertFalse(prime_num(-7))
-        self.assertFalse(prime_num(-100))
+        self.assertFalse(prime_num(-20) == False)
+        self.assertFalse(prime_num(-7) == False)
+        self.assertFalse(prime_num(-100) == False)
     def test_zero(self):
         '''Test that zero is not prime.'''
-        self.assertFalse(prime_num(0))
+        self.assertFalse(prime_num(0) == False)
     def test_one(self):
         '''Test that 1 is not prime.'''
-        self.assertFalse(prime_num(1))
+        self.assertFalse(prime_num(1) == False)
     def test_composites(self):
         '''Test that several composites are not prime.'''
-        self.assertFalse(prime_num(12))
-        self.assertFalse(prime_num(16))
-        self.assertFalse(prime_num(25))
+        self.assertFalse(prime_num(12) == False)
+        self.assertFalse(prime_num(16) == False)
+        self.assertFalse(prime_num(25) == False)
 
 if __name__ == '__main__':
     unittest.main()def tests (self):
